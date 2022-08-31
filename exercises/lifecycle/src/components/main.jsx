9fdc2e0ccac72c9a48d6c6a959548ee8bbe06822
jariful hoque <jarifulhoque@hotmail.co.uk>
2022-08-31 23:54:56 +0100

    clock

---
+++ exercises/lifecycle/src/components/main.jsx
@@ -0,0 +1,14 @@
+import Clock from './clock.jsx';
+
+const Main = () => {
+    return (
+        <div className="App-main">
+            <h2>Lifecycle ex</h2>
+            <div className="output" height="10vh;">
+                <Clock />
+            </div>
+        </div>
+    );
+}
+
+export default Main; 
